d7a51fa22f9150249bdc7860cd64162d49e7b479
mak48 <makoveevaanastasija@yandex.ru>
2025-05-23 16:52:38 +0300

    Updates

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  apiUrl:
-    process.env.NODE_ENV === "production"
-      ? "https://coursework-latest-uayr.onrender.com"
-      : "http://localhost:8080",
-};
-
-export default config;
--- src/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  testMatch: ["<rootDir>/src/**/*.test.js"],
-  moduleNameMapper: {
-    axios: "axios/dist/node/axios.cjs",
-  },
-};
